340150db861f93f9a825ca8e0ca3bc6d326c3379
Jeffreasy <158185183+Jeffreasy@users.noreply.github.com>
2026-01-31 17:19:22 +0100

    chore: Remove console logs from Service Workers

--- public/sw.js
+++ public/sw.js
@@ -1,35 +1,25 @@
 const CACHE_VERSION = 'v1-destroy-2026-01-31';
 
 self.addEventListener('install', (event) => {
-    console.log('[SW] Installing cleanup worker', CACHE_VERSION);
     // Force immediate activation
     self.skipWaiting();
 });
 
 self.addEventListener('activate', (event) => {
-    console.log('[SW] Activating cleanup worker', CACHE_VERSION);
     event.waitUntil(
         // 1. Delete ALL caches
-        caches.keys().then(cacheNames => {
+        caches.keys()
+        .then(cacheNames => {
             return Promise.all(
-                cacheNames.map(cacheName => {
-                    console.log('[SW] Deleting cache:', cacheName);
-                    return caches.delete(cacheName);
-                })
+                cacheNames.map(cacheName => caches.delete(cacheName))
             );
         })
         // 2. Unregister this service worker
-        .then(() => {
-            console.log('[SW] Unregistering service worker');
-            return self.registration.unregister();
-        })
+        .then(() => self.registration.unregister())
         // 3. Force reload all clients
         .then(() => self.clients.matchAll())
         .then(clients => {
-            clients.forEach(client => {
-                console.log('[SW] Reloading client:', client.url);
-                client.navigate(client.url);
-            });
+            clients.forEach(client => client.navigate(client.url));
         })
     );
 });
